14840cdc4d151968e291262eb2c0a9030c96db3b
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2024-05-15 12:47:08 -0400

    Add React frontend - initial commit

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type habit struct {
+	id        string `json:"id"`
+	name      string `json:"name"`
+	completed bool   `json:"completed"`
+}
+
+var habits = []habit{
+	{id: "1", name: "Work Out", completed: false},
+	{id: "2", name: "Leet Code", completed: false},
+	{id: "3", name: "Work on Project", completed: false},
+}
+
+func getHabits(context *gin.Context) {
+	return context.IndentedJSON(http.StatusOK, habits)
+}
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+	router.Run("localhost:9090")
+	router.GET("/habits", getHabits)
+	fmt.Println("This is the start of the backend")
 }
